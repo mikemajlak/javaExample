30e9b4a12202ef5cc8cd48b44482f161ed2a6ee1
mikemajlak <majlak@1234.com>
2023-08-03 12:53:25 +0530

    Added a method printString(String) in Example class which prints any string which is passed to it

--- Example.java
+++ Example.java
@@ -1,6 +1,9 @@
 
 
-public class Example{
+public class Example {
+    public void printString(String str){
+        System.out.print(str);
+    }
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
