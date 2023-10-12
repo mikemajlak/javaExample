1487dd821e9ff11bc94634e75025fcd53d2d1173
mikemajlak <majlak@1234.com>
2023-10-12 09:29:17 +0530

    added a new method for subtracting two integer

--- Example.java
+++ Example.java
@@ -4,6 +4,10 @@ public class Example {
     public float divide(float a, float b) {
         return a / b;
     }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
     public int Multiply(int a, int b) {
         return a * b;
     }
